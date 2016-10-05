92acba3681677d6323921c0e535ef218e84fa29e
whtsky <whtsky@gmail.com>
2016-10-06 00:35:27 +0800

    Ahh... Crystal crashed on my Mac

--- src/B/api.cr
+++ src/B/api.cr
@@ -8,14 +8,14 @@ module B
 
   get "/transaction" do |env|
     env.response.content_type = "application/json"
-    datas = [] of Hash(String, Int32 | String)
+    datas = [] of Hash(String, Int64 | String)
     DB.open DATA_URL do |db|
       db.query "select id, amount, time, ip, ua, description from transactions order by time desc" do |rs|
         rs.each do
           datas << {
-            "id" => rs.read(Int32),
-            "amount" => rs.read(Int32),
-            "time" => rs.read(Int32),
+            "id" => rs.read(Int64),
+            "amount" => rs.read(Int64),
+            "time" => rs.read(Int64),
             "ip" => rs.read(String),
             "ua" => rs.read(String),
             "description" => rs.read(String)
@@ -24,5 +24,21 @@ module B
       end
     end
     datas.to_json
+  end
+
+  post "/transaction" do |env|
+    env.response.content_type = "application/json"
+    time = env.params.body["time"]
+    begin
+      
+    rescue
+      
+    end
+    
+    begin
+
+    rescue
+
+    end
   end
 end
